90d928a61ef2640f4d1fd95d974c24aada73d55b
Sohaib Awan <43108373+Cheebo12@users.noreply.github.com>
2021-04-19 16:22:45 +0500

    Sky Ball
    A 3D game platform game Where you move the ball to get past levels with each level getting more and more challenging and exciting

---
+++ SkyBall/Assets/Scripts/jump.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{   private Rigidbody rb;
+public ParticleSystem dust;
+    public float JumpForce = 5;
+    public bool grounded;
+    public LayerMask groundlayer;
+    public float groundlength;
+    private SphereCollider col;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
+        grounded = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began && grounded )
+            {
+                rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);                
+                grounded = false;
+                dustplay();
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision col){
+        if(col.gameObject.tag == "Floor"){
+            grounded = true;
+        }
+        
+    }
+    void dustplay(){
+        dust.Play();
+    }
+}
